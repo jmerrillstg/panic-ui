8c7d6b0f35ae46cd2503f537ce2ee280e84d3466
Jason Merrill <jason.merrill@stgconsulting.com>
2017-07-11 14:19:02 -0600

    code cleanup, addition of moment and recipient management

--- source/components/account/account-controller.js
+++ source/components/account/account-controller.js
@@ -14,7 +14,7 @@ export default function ($http, $rootScope, appConfig) {
     function updateUser() {
         $http({
             method: 'PUT',
-            url: appConfig.apiUrl+'/profile/'+$rootScope.userId,
+            url: appConfig.apiUrl+'/profile',
             data: acc.profile,
             headers : {'Content-Type': 'application/json'}
         })
