d6b7d86171895e1b49d4e7196fbea7fbc2970902
Jason Merrill <jason.merrill@stgconsulting.com>
2017-07-11 16:26:47 -0600

    added the ability to edit the message

--- source/components/home/home-controller.js
+++ source/components/home/home-controller.js
@@ -1,3 +1,31 @@
-export default function () {
+export default function ($http, appConfig) {
+    let hc = this;
 
+    hc.error = '';
+    hc.success = false;
+
+    $http.get(appConfig.apiUrl+'/message').
+    then(function(response) {
+        hc.message = response.data[0].message_text;
+    }, function () {
+        hc.error='Failed to retrieve message';
+    });
+
+    function updateMessage() {
+        $http({
+            method: 'PUT',
+            url: appConfig.apiUrl+'/message',
+            data: {message_text: hc.message},
+            headers : {'Content-Type': 'application/json'}
+        })
+        .then(function() {
+            hc.success = true;
+            hc.successMessage = 'Message Updated Successfully';
+        },
+        function() {
+            hc.error = 'Message Update Failed';
+        });
+    }
+
+    hc.updateMessage = updateMessage;
 }
